50c247e1f92f8a48cdcd1033cc7ae2c11ed44b24
Sum Daniel <daniel@sum.consulting>
2025-01-13 16:27:15 +0100

    feat: hide description field

--- src/app/dashboard/profile/_components/profile-create-form.tsx
+++ src/app/dashboard/profile/_components/profile-create-form.tsx
@@ -20,11 +20,11 @@ import { z } from 'zod';
 export const profileSchema = z.object({
   name: z.string().min(2, 'Name must be at least 2 characters'),
   email: z.string().email('Invalid email address'),
-  phone: z.string().min(2, 'Invalid phone number'),
-  description: z
-    .string()
-    .min(2, 'Description must be at least 10 characters')
-    .max(500, 'Description must not exceed 500 characters')
+  phone: z.string().min(2, 'Invalid phone number')
+  // description: z
+  //   .string()
+  //   .min(2, 'Description must be at least 10 characters')
+  //   .max(500, 'Description must not exceed 500 characters')
 });
 
 export type ProfileFormData = z.infer<typeof profileSchema>;
