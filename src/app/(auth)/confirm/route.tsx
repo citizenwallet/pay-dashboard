0e972a6e0f93baf3375ac342896dbe2f1f7447c0
Sum Daniel <daniel@sum.consulting>
2025-01-22 18:36:27 +0100

    feat: fix login

--- src/app/(auth)/confirm/route.tsx
+++ src/app/(auth)/confirm/route.tsx
@@ -1,6 +1,5 @@
 import { type EmailOtpType } from '@supabase/supabase-js';
-import { type NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
+import { NextResponse, type NextRequest } from 'next/server';
 import { createClient } from '@/lib/supabase/server';
 import { redirect } from 'next/navigation';
 import { signIn } from '@/auth';
@@ -11,7 +10,6 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get('token_hash');
   const type = searchParams.get('type') as EmailOtpType | null;
-  const next = searchParams.get('next') ?? '/';
 
   if (token_hash && type) {
     const supabase = await createClient();
@@ -39,14 +37,14 @@ export async function GET(request: NextRequest) {
           email: email
         });
 
-        return NextResponse.redirect('/dashboard');
+        return NextResponse.redirect(process.env.NEXTAUTH_URL + '/dashboard');
       } else {
-        return NextResponse.redirect('/dashboard');
+        return NextResponse.redirect(process.env.NEXTAUTH_URL + '/dashboard');
       }
     } else {
-      return NextResponse.redirect('/dashboard');
+      return NextResponse.redirect(process.env.NEXTAUTH_URL + '/dashboard');
     }
   } else {
-    return NextResponse.redirect('/dashboard');
+    return NextResponse.redirect(process.env.NEXTAUTH_URL + '/dashboard');
   }
 }
