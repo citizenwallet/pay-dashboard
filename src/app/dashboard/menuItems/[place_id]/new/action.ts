29328961e0ded273e17f90ab12911dd48587ef52
Randika Dilshan <rdilshan077788@gmail.com>
2025-02-28 11:41:54 +0530

    new item add

--- src/app/dashboard/menuItems/[place_id]/new/action.ts
+++ src/app/dashboard/menuItems/[place_id]/new/action.ts
@@ -4,6 +4,9 @@ import { getServiceRoleClient } from '@/db';
 import { InsertItem } from '@/db/items';
 import NextAuth from 'next-auth';
 import authConfig from '@/auth.config';
+import { uploadImage } from '@/db/image';
+import { getUserBusinessId } from '@/db/users';
+import { checkUserPlaceAccess } from '@/db/places';
 
 const { auth } = NextAuth(authConfig);
 
@@ -17,21 +20,35 @@ export default interface createItemschema {
   place_id: number;
 }
 
-
-
 export const creatItem = async (item: createItemschema) => {
   const client = getServiceRoleClient();
   const user = await auth();
+
+  const res = await checkUserPlaceAccess(
+    client,
+    Number(user?.user?.id),
+    item.place_id
+  );
+  if (!res) {
+    throw new Error('User does not have access to this place');
+  }
+
+  const business_id = await getUserBusinessId(client, Number(user?.user?.id));
+  const imageUrl = await uploadImage(
+    client,
+    item.image,
+    Number(business_id),
+    item.place_id
+  );
   const response = await InsertItem(
     client,
     item.name,
     item.description,
-    item.image,
+    imageUrl,
     item.price,
     item.vat,
     item.category,
-    item.place_id,
-    Number(user?.user?.id)
+    item.place_id
   );
   return response;
 };
