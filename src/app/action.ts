e245a9feaaa0f1a9e7b88f90b32a34932417f0ca
Randika Dilshan <rdilshan077788@gmail.com>
2025-03-04 12:41:37 +0530

    provided for the first time route

---
+++ src/app/action.ts
@@ -0,0 +1,37 @@
+'use server';
+
+import { getUserIdFromSessionAction } from '@/actions/session';
+import { getServiceRoleClient } from '@/db';
+import { getBusinessIdByUserId } from '@/db/business';
+import { getLastPlaceAction } from './business/action';
+import { getPlaceById, getPlacesByBusinessId } from '@/db/places';
+import { getLastplace } from '@/db/users';
+
+export async function getPlaceAllAction() {
+  const client = getServiceRoleClient();
+  const userId = await getUserIdFromSessionAction();
+  const businessid = await getBusinessIdByUserId(client, userId);
+  const places = await getPlacesByBusinessId(
+    client,
+    businessid.data?.linked_business_id
+  );
+  return places.data;
+}
+
+export async function getPlaceAction(): Promise<{ lastId: number; busid: number }> {
+    const client = getServiceRoleClient();
+    const userId = await getUserIdFromSessionAction();
+    const businessid = await getBusinessIdByUserId(client, userId);
+    const busid:number = businessid.data?.linked_business_id; // output
+  
+    const data = await getLastplace(client, userId);
+    let lastId: number = data.data?.last_place;
+    if (!lastId) {
+      const places = await getPlaceAllAction();
+      if (places && places.length > 0) {
+        lastId = places[0].id;
+      }
+    }
+  
+    return { lastId, busid };
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
+import { getPlaceAction } from './action';
 
 export default async function Page() {
   const session = await auth();
@@ -7,6 +8,7 @@ export default async function Page() {
   if (!session?.user) {
     return redirect('/login');
   } else {
-    return redirect('/dashboard');
+    const data = await getPlaceAction();
+    return redirect(`/business/${data.busid}/places/${data.lastId}/orders`);
   }
 }
