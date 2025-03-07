b7b45c36e8503eecbb3f5d368a06493485f13f21
kev-techi <kevin@techi.be>
2025-03-07 14:33:57 +0100

    fix build issues

--- src/app/business/[businessId]/places/[placeId]/manage/page.tsx
+++ src/app/business/[businessId]/places/[placeId]/manage/page.tsx
@@ -1,13 +1,15 @@
-import PageContainer from "@/components/layout/page-container";
-import { Heading } from "@/components/ui/heading";
-import { Separator } from "@/components/ui/separator";
-import { Suspense } from "react";
-import ManagePage from "./managePage";
-import { getPlaceDataAction, getPlaceOrderAction } from "./action";
+import PageContainer from '@/components/layout/page-container';
+import { Heading } from '@/components/ui/heading';
+import { Separator } from '@/components/ui/separator';
+import { Suspense } from 'react';
+import ManagePage from './managePage';
+import { getPlaceDataAction, getPlaceOrderAction } from './action';
 
-
-export default async function page({ params }: { params: { businessId: string, placeId: string } }) {
-
+export default async function page({
+  params
+}: {
+  params: Promise<{ businessId: string; placeId: string }>;
+}) {
   const resolvedParams = await params;
   return (
     <>
@@ -17,19 +19,27 @@ export default async function page({ params }: { params: { businessId: string, p
             <Heading title="Manage" description="Manage your Place action" />
           </div>
           <Separator />
-          <Suspense
-            fallback={<div>Loading...</div>}>
-            <AsyncPage businessId={resolvedParams.businessId} placeId={resolvedParams.placeId} />
+          <Suspense fallback={<div>Loading...</div>}>
+            <AsyncPage
+              businessId={resolvedParams.businessId}
+              placeId={resolvedParams.placeId}
+            />
           </Suspense>
         </div>
       </PageContainer>
     </>
-  )
+  );
 }
 
-async function AsyncPage({ businessId, placeId }: { businessId: string, placeId: string }) {
+async function AsyncPage({
+  businessId,
+  placeId
+}: {
+  businessId: string;
+  placeId: string;
+}) {
   const place = await getPlaceDataAction(parseInt(placeId));
   const order = await getPlaceOrderAction(parseInt(placeId));
   const count = order.data?.length;
-  return <ManagePage place={place.data} orderPlace={count} />
+  return <ManagePage place={place.data} orderPlace={count} />;
 }
