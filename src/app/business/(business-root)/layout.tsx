4cc6e8f56079aa15b77b202286f9d0612955f538
Randika Dilshan <rdilshan077788@gmail.com>
2025-03-12 07:01:43 +0530

    Create the business root sidebar.

--- src/app/business/(business-root)/layout.tsx
+++ src/app/business/(business-root)/layout.tsx
@@ -1,12 +1,31 @@
-export default function BusinessRootLayout({
+import {
+  getUserIdFromSessionAction,
+  isUserAdminAction
+} from '@/actions/session';
+import { getLinkedBusinessAction } from '../(business-details)/[businessId]/action';
+import { getServiceRoleClient } from '@/db';
+import { getUserById } from '@/db/users';
+import RootAppSidebar from '@/components/layout/root-sidebar';
+
+export default async function BusinessRootLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
+  const admin = await isUserAdminAction();
+  const business = await getLinkedBusinessAction();
+  const userId = await getUserIdFromSessionAction();
+  const client = getServiceRoleClient();
+  const { data: user } = await getUserById(client, userId);
+  if (!user) {
+    return null;
+  }
+
   return (
-    <div>
-      <h1>Business Root Layout</h1>
-      {children}
-    </div>
+    <>
+      <RootAppSidebar business={business} isAdmin={admin} user={user}>
+        {children}
+      </RootAppSidebar>
+    </>
   );
 }
