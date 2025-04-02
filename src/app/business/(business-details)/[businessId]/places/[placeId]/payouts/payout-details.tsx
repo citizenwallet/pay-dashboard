ce46fcfebd7276d8994dbeeec68500a397fd8308
Randika Dilshan <rdilshan077788@gmail.com>
2025-04-02 09:06:30 +0530

    Add translations for payouts page

--- src/app/business/(business-details)/[businessId]/places/[placeId]/payouts/payout-details.tsx
+++ src/app/business/(business-details)/[businessId]/places/[placeId]/payouts/payout-details.tsx
@@ -4,6 +4,7 @@ import CurrencyLogo from '@/components/currency-logo';
 import { DataTable } from '@/components/ui/data-table';
 import { Payout } from '@/db/payouts';
 import { formatCurrencyNumber } from '@/lib/currency';
+import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 
 export default function PayoutDetailsPage({
@@ -17,13 +18,14 @@ export default function PayoutDetailsPage({
   businessId: string;
   currencyLogo: string;
 }) {
+  const t = useTranslations('payouts');
   return (
     <>
       <DataTable
         columns={[
           {
             accessorKey: 'id',
-            header: 'Id',
+            header: t('id'),
             cell: ({ row }) => (
               <Link
                 href={`/business/${businessId}/places/${placeId}/payouts/${row.original.id}`}
@@ -34,19 +36,19 @@ export default function PayoutDetailsPage({
           },
           {
             accessorKey: 'from',
-            header: 'From',
+            header: t('from'),
             cell: ({ row }) =>
               new Date(row.original.from).toLocaleDateString('en-GB')
           },
           {
             accessorKey: 'to',
-            header: 'To',
+            header: t('to'),
             cell: ({ row }) =>
               new Date(row.original.to).toLocaleDateString('en-GB')
           },
           {
             accessorKey: 'total',
-            header: 'Total',
+            header: t('total'),
             cell: ({ row }) => {
               return (
                 <p className="flex w-8 items-center gap-1">
@@ -59,7 +61,7 @@ export default function PayoutDetailsPage({
 
           {
             accessorKey: 'burn',
-            header: 'Burn',
+            header: t('burn'),
             cell: ({ row }) => (
               <div className="flex items-center space-x-2">
                 <span className="text-red-500">
@@ -78,7 +80,7 @@ export default function PayoutDetailsPage({
 
           {
             accessorKey: 'transfer',
-            header: 'Transfer',
+            header: t('transfer'),
             cell: ({ row }) => (
               <div className="flex items-center space-x-2">
                 <span className="text-blue-500">
