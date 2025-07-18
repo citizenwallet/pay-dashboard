3b783ea4fb240d0833e2a1c1099e1577af3337bc
Randika Dilshan <rdilshan077788@gmail.com>
2025-07-18 17:40:25 +0530

    refactor UserNav component to use user object

--- src/components/layout/user-nav.tsx
+++ src/components/layout/user-nav.tsx
@@ -12,32 +12,35 @@ import {
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu';
 import { Place } from '@/db/places';
+import { User } from '@/db/users';
 import { signOut, useSession } from 'next-auth/react';
 import { useTranslations } from 'next-intl';
 import { useRouter } from 'next/navigation';
 
 export function UserNav({
   businessId,
-  lastPlace
+  lastPlace,
+  user
 }: {
   businessId?: number;
   lastPlace?: Place
+  user: User
 }) {
-  const { data: session } = useSession();
+
   const router = useRouter();
   const t = useTranslations('userNav');
 
-  if (session) {
+  if (user) {
     return (
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="relative h-8 w-8 rounded-full">
             <Avatar className="h-8 w-8">
               <AvatarImage
-                src={session.user?.image ?? ''}
-                alt={session.user?.name ?? ''}
+                src={user.avatar ?? ''}
+                alt={user.name ?? ''}
               />
-              <AvatarFallback>{session.user?.name?.[0]}</AvatarFallback>
+              <AvatarFallback>{user.name?.[0]}</AvatarFallback>
             </Avatar>
           </Button>
         </DropdownMenuTrigger>
@@ -45,10 +48,10 @@ export function UserNav({
           <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
               <p className="text-sm font-medium leading-none">
-                {session.user?.name}
+                {user.name}
               </p>
               <p className="text-xs leading-none text-muted-foreground">
-                {session.user?.email}
+                {user.email}
               </p>
             </div>
           </DropdownMenuLabel>
