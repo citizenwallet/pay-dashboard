5890514d6a25d98dabb5090375502a0328dc7db8
Randika Dilshan <rdilshan077788@gmail.com>
2025-07-23 11:12:26 +0530

    Add cron job to sync place balances every 15 minutes and implement related API route

---
+++ src/app/api/cron/place-balance/route.ts
@@ -0,0 +1,49 @@
+'use server';
+import Config from '@/cw/community.json';
+import { getServiceRoleClient } from '@/db';
+import { upsertPlaceBalance } from '@/db/placeBalance';
+import { getAllPlacesWithBusiness } from '@/db/places';
+import { CommunityConfig, getAccountBalance } from '@citizenwallet/sdk';
+import { StatusCodes } from 'http-status-codes';
+import { NextRequest, NextResponse } from 'next/server';
+
+// This API route will be called by a cron job every 15 minutes
+export async function POST(req: NextRequest) {
+  try {
+    //update the place balance
+    const client = getServiceRoleClient();
+    const { data } = await getAllPlacesWithBusiness(client);
+    const community = new CommunityConfig(Config);
+    const token = community.primaryToken.address;
+    if (data) {
+      data.map(async (place) => {
+        const balance = await getAccountBalance(community, place.accounts[0]);
+        await upsertPlaceBalance(client, {
+          token: token,
+          place_id: place.id,
+          balance: Number(balance)
+        });
+      });
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: 'Business sync cron job completed successfully',
+        timestamp: new Date().toISOString()
+      },
+      { status: StatusCodes.OK }
+    );
+  } catch (error) {
+    console.error('Business sync cron job failed:', error);
+
+    return NextResponse.json(
+      {
+        success: false,
+        error: error instanceof Error ? error.message : 'Unknown error',
+        timestamp: new Date().toISOString()
+      },
+      { status: StatusCodes.INTERNAL_SERVER_ERROR }
+    );
+  }
+}
