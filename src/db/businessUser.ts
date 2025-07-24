bfd8ab81a44a23aa700066f7038317a9a8fe5649
Randika Dilshan <rdilshan077788@gmail.com>
2025-07-24 16:42:55 +0530

    Add isOwnerOfBusiness function to check user ownership of a business

--- src/db/businessUser.ts
+++ src/db/businessUser.ts
@@ -20,3 +20,22 @@ export const createBusinessUser = async (
     .select()
     .single();
 };
+
+export const isOwnerOfBusiness = async (
+  client: SupabaseClient,
+  user_id: number,
+  business_id: number
+): Promise<boolean> => {
+  const { data, error } = await client
+    .from('business_users')
+    .select('*')
+    .eq('user_id', user_id)
+    .eq('business_id', business_id)
+    .single();
+
+  if (error) {
+    return false;
+  }
+
+  return data?.role === 'owner';
+};
